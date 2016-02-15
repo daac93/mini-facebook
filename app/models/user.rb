a8902732ccedc835552bb1671c0b6b90f0423020
Daniel Arguedas <daac93@gmail.com>
2016-02-15 17:47:52 +0000

    Added first and last name to devise new and edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,39 @@ class User < ActiveRecord::Base
     return "#{first_name} #{last_name}".strip if (first_name || last_name)
     "Anonymous"
   end
+  
+  def has_pending_friend_requests?
+    
+  end
+  
+  def can_like_post?(post_id)
+  end
+  
+  def not_friends_with?(friend_id)
+    #friendships.where(friend_id: friend_id).count < 1
+  end
+  
+  def self.search(param)
+    return User.none if param.blank?
+    
+    param.strip!
+    param.downcase!
+    (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+  end
+  
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+  
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+  
+  def self.email_matches(param)
+    matches('email', param)
+  end
+  
+  def self.matches(field_name, param)
+    where("lower(#{field_name}) like ?", "%#{param}%")
+  end
 end
