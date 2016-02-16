ffb64d774d85bba6ed5914ee6f38d0142992c6aa
Daniel Arguedas <daac93@gmail.com>
2016-02-16 00:55:40 +0000

    Updated navigation. Added posts skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :posts
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'pages#homepage'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  get 'my_profile' => 'users#my_profile'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
